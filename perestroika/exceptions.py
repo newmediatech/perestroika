04dc16f1ddbb700bd60f8c1a464f3100916e0874
Ruslan Roskoshny <i.am.yarger@gmail.com>
2018-12-10 17:57:54 +0300

    Exceptions string

--- perestroika/exceptions.py
+++ perestroika/exceptions.py
@@ -3,10 +3,16 @@ class RestException(BaseException):
         self.message = message
         self.status_code = status_code
 
+    def __str__(self):
+        return f"RestException: {self.message}"
+
 
 class ValidationException(BaseException):
     def __init__(self, message=None) -> None:
         self.message = message
+
+    def __str__(self):
+        return f"ValidationException: {self.message}"
 
 
 class BadRequest(RestException):
